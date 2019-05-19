b7c805336bcedaa32e4ccb10f0b088809603858a
leonel <leonellrodriguez@gmail.com>
2019-05-19 12:35:49 -0500

    Primer commit

---
+++ precache-manifest.718c7d6ff2babff13e35706f7c59b3f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca8770b25809cbe0d0a6a620059c59c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba436eea8d34f628ef8e",
+    "url": "/static/css/main.0010486f.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "ba436eea8d34f628ef8e",
+    "url": "/static/js/main.2833f90a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
